e5bd833141d8db4c61133c5c09b9d4e3db8d63a0
rohitsingh341 <rohitsingh341@gmail.com>
2022-11-02 16:40:11 +0000

    Possible solutions in comment section

--- src/ContainsDuplicateElement.java
+++ src/ContainsDuplicateElement.java
@@ -12,6 +12,11 @@ public class ContainsDuplicateElement {
             System.out.println("Does not contain duplicates");
     }
 
+    // Solution 1 - Brute force - iterate items and check  them with other elements - Time -> O(n*2), Space -> O(1)
+
+    // Solution 2 - Sort the array first and then iterate and compare - Time -> O(nLogn), Space -> O(1)
+
+    // Solution 3 - Use HashSet, Add item in the set and then add() operation will return false if the element is already there.
     private static boolean containsDuplicate(int[] nums) {
         Set<Integer> numSet = new HashSet<>();
         for (int n: nums) {
